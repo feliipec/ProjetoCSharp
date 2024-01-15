3b272bfc1548479e6c4d18dcbf0fea8bbe8ecb4b
Felipe Correia <feliipecorreiia@gmail.com>
2024-01-15 22:45:55 +0000

    ProjetoInicial

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Program {
+    class Program {
+        static void Main (string[]args) {
+            sbyte n1 = 126;
+            int n2 = 1000;
+        
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+        }
+    }
+}
