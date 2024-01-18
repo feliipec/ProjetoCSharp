38c27ed79d06142656e2dc460bb7b805df49a8e0
Felipe Correia <feliipecorreiia@gmail.com>
2024-01-18 23:37:06 +0000

    FormulaDeBhaskara

--- Program.cs
+++ Program.cs
@@ -4,26 +4,11 @@ using System.Globalization;
 namespace Course {
     class Program {
         static void Main(string[]args) {
-
-            string produto1 = "Computador";
-            string produto2 = "Mesa de Escritório";
-            byte idade = 30;
-            int codigo = 5290;
-            char genero = 'M';
-            double preco1 = 2100.0;
-            double preco2 = 650.50;
-            double medida = 53.234567;
-
-            Console.WriteLine("Produtos:");
-            Console.WriteLine($"{produto1}, cujo preço é R$ {preco1}");
-            Console.WriteLine($"{produto2}, cujo preço é R$ {preco2}");
-            Console.WriteLine();
-            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e genero {genero}");
-            Console.WriteLine();
-            Console.WriteLine($"Medida com oito casas decimais: {medida:F8}");
-            Console.WriteLine($"Arredondado(três casas decimais): {medida:F3}");
-
-           
+            double a = 1.0, b = -3.0, c = -4.0;
+            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+            double x1  = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            Console.WriteLine(delta);
+            Console.WriteLine(x1);
         }
     }
 }
--- obj/Debug/net8.0/ProjetoCSharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjetoCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b272bfc1548479e6c4d18dcbf0fea8bbe8ecb4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0375dfb04f5022cd4e6c6a31ef199b1f4474bca6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
